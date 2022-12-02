ab9e9b63d25c6cca214bcf0ed250954cc53d47bd
Yizhu Zong <agnes.lain@gmail.com>
2022-12-02 17:27:12 +0100

    update

--- day-01.rb
+++ day-01.rb
@@ -2,18 +2,13 @@ require_relative 'common'
 
 class Day1 < AdventDay
   def first_part
-    a = input.join(",").split(",,")
-    na = a.map{|e| e.split(",").map{|s| s.to_i}}
-    c = na.map{|arr| arr.sum}
-    c.max
+    a = cal(input)
+    a.max
   end
 
   def second_part
-    a = input.join(",").split(",,")
-    na = a.map{|e| e.split(",").map{|s| s.to_i}}
-    c = na.map{|arr| arr.sum}
-    first = c.max
-    c.sort[-3..-1].sum
+    a = cal(input)
+    a.max(3).sum
   end
 
   private
@@ -24,6 +19,13 @@ class Day1 < AdventDay
 
   def debug_input
     "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
+  end
+
+  def cal(input)
+    a = input.join(",").split(",,")
+    na = a.map{|e| e.split(",").map{|s| s.to_i}}
+    c = na.map{|arr| arr.sum}
+    c
   end
 end
 
