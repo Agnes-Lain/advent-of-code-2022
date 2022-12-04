99657ac2b0c4b237ac37375cc179b7464c25738f
Yizhu Zong <agnes.lain@gmail.com>
2022-12-04 23:32:29 +0100

    day 3 and day 4

--- day-03.rb
+++ day-03.rb
@@ -1,10 +1,42 @@
 require_relative 'common'
+require 'byebug'
 
 class Day3 < AdventDay
   def first_part
+    arr = input.map{|e| [e[0...e.size/2].chars, e[e.size/2..-1].chars]}
+    new_a = []
+    arr.each do |a|
+      sub_a = []
+      a[0].each do |l|
+        # byebug
+        sub_a << l if a[1].include?(l)
+      end
+      new_a << sub_a.uniq[0]
+    end
+    # new_a
+    new_a.map{ |l| l.bytes[0] > 96 ? l.bytes[0] - 96 : l.downcase.bytes[0] - 70 }.sum
   end
 
   def second_part
+    input
+    # byebug
+    input_ = input.map { |s| s.chars }
+    index = 0
+    arr = []
+    while index < input_.size
+      arr << input_[index..index + 2]
+      index += 3
+    end
+    new_a = []
+    arr.each do |a|
+      sub_a = []
+      a[0].each do |l|
+        sub_a << l if a[1].include?(l) && a[2].include?(l)
+      end
+      new_a << sub_a.uniq[0]
+    end
+    # new_a
+    new_a.map{ |l| l.bytes[0] > 96 ? l.bytes[0] - 96 : l.downcase.bytes[0] - 70 }.sum
   end
 
   private
@@ -12,6 +44,11 @@ class Day3 < AdventDay
   def convert_data(data)
     super
   end
+
+  def debug_input
+    "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw"
+  end
+
 end
 
 Day3.solve
--- day-04.rb
+++ day-04.rb
@@ -1,10 +1,31 @@
 require_relative 'common'
+require 'byebug'
 
 class Day4 < AdventDay
   def first_part
+    arr = input.map { |e| e.split(",") }
+    arr1 = arr.map { |a| a.map { |e| e.split('-') }}
+    count = 0
+    arr1.each do |a|
+      # byebug
+      sub_a = (a[0][0].to_i..a[0][1].to_i).to_a
+      sub_b = (a[1][0].to_i..a[1][1].to_i).to_a
+      count += 1 if (sub_a - sub_b).empty? || (sub_b - sub_a).empty?
+    end
+    count
   end
 
   def second_part
+    arr = input.map { |e| e.split(",") }
+    arr1 = arr.map { |a| a.map { |e| e.split('-') }}
+    count = 0
+    arr1.each do |a|
+      # byebug
+      sub_a = (a[0][0].to_i..a[0][1].to_i).to_a
+      sub_b = (a[1][0].to_i..a[1][1].to_i).to_a
+      count += 1 if (sub_a - sub_b) != sub_a
+    end
+    count
   end
 
   private
@@ -12,6 +33,11 @@ class Day4 < AdventDay
   def convert_data(data)
     super
   end
+
+  def debug_input
+    "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
+  end
+
 end
 
 Day4.solve
