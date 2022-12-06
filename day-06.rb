97e0bb47e79255423be338aa6018f473a7dd9dea
Yizhu Zong <agnes.lain@gmail.com>
2022-12-06 10:13:15 +0100

    day 6

--- day-06.rb
+++ day-06.rb
@@ -1,10 +1,37 @@
 require_relative 'common'
+require 'byebug'
 
 class Day6 < AdventDay
   def first_part
+    arr = []
+    input[0].each_char do |letter|
+      if arr.size < 4
+        # byebug
+        arr << letter
+      else
+        if arr.last(4).uniq.size == 4
+          return arr.size
+        else
+          arr << letter
+        end
+      end
+    end
   end
 
   def second_part
+    arr = []
+    input[0].each_char do |letter|
+      if arr.size < 14
+        # byebug
+        arr << letter
+      else
+        if arr.last(14).uniq.size == 14
+          return arr.size
+        else
+          arr << letter
+        end
+      end
+    end
   end
 
   private
@@ -14,7 +41,7 @@ class Day6 < AdventDay
   end
 
   def debug_input
-    ""
+    "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
   end
 
 end
